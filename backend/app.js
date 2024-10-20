95fc271824b5a105217ac9c972b42bdc17fb9b56
saroubel <bellaf.sara@gmail.com>
2024-10-20 03:17:05 +0200

    Add Middleware

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
-//import express et appel methode express qui est dans serveur.js
-const express = require('express');     
-const app = express();                  
+const express = require('express');     //importer framework pour NodeJs
+const app = express();                  // appel cette méthode pour configuration de serveur
 
-// Middleware pour 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
 
 // Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
-module.exports = app;   //exporter app pour y accèder depuis d'autres fichiers 
+// Middleware pour gérer les headers CORS (Cross-Origin Resource Sharing)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Autorise les requêtes depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Autorise certains en-têtes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Autorise certaines méthodes HTTP
+    next();
+  });
 
+// Middleware pour gestion des erreurs 404
+app.use((req, res, next) => {
+    res.status(404).send("Not Found");
+    next();
+});
+
+//Exporter app 
+module.exports = app;
