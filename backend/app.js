cc1490e7576077e09092295f53bb78227a8bc01c
saroubel <bellaf.sara@gmail.com>
2024-10-21 20:59:40 +0200

    install dotenv & Mongodb Connection

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
-const express = require('express');     //importer framework pour NodeJs
-const app = express();                  // appel cette méthode pour configuration de serveur
+const express = require('express');                  //framework pour Node.js
+const app = express();                               //méthode pour configuration de serveur
+const mongoose = require('mongoose');                //biblio ODM (Object Data Modeling) qui facilite les opérations CRUD
+const path = require('path');                        //manipuler les chemins de fichiers
+require("dotenv").config({ path: ".env" });          //Charge les variables d'environnement
+// const userRoutes = require('./routes/user');         
+// const bookRoutes = require('./routes/book');
+
 
 
 // Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
-// Middleware pour gérer les headers CORS (Cross-Origin Resource Sharing)
+// Middleware pour gérer les headers CORS 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Autorise les requêtes depuis n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Autorise certains en-têtes
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Accède à l'API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Autorise certains headers
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Autorise certaines méthodes HTTP
     next();
   });
@@ -18,6 +24,19 @@ app.use((req, res, next) => {
     res.status(404).send("Not Found");
     next();
 });
+
+
+// Connexion à la base de données MongoDB
+mongoose.connect(process.env.MONGODB_CLUSTER_URL,
+    { useNewUrlParser: true, })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// Routes
+// app.use('/api/auth', userRoutes);
+// app.use('/api/books', bookRoutes);
+
 
 //Exporter app 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,8 @@
-// const http = require('http');
-// const app = require('./app');
-
-// app.set('port', process.env.PORT || 4000);
-// const server = http.createServer(app);
-
-// //serveur en ecoute soit variable d'environnement ou 4000 par defaut
-// server.listen(process.env.PORT || 4000); 
-// console.log('Server running on port 4000');
-
-
 const http = require('http');
 const app = require('./app');
 
-// Fonction pour normaliser le port que l'application utilise
-// elle renvoie port valide soit en format numérique ou chaine
+
+// Fonction pour normaliser le port que l'app utilise soit en format num ou chaine
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -26,9 +15,11 @@ const normalizePort = val => {
   return false;
 };
 
-// Définir le port de l'app dans les variables d'environnement ou 4000 par défaut
+
+// Définir le port dans les variables d'environnement ou 4000 par défaut
 const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
+
 
 // Fonction pour gérer les erreurs lors du démarrage du serveur
     // Si l'erreur n'est pas liée à l'écoute du serveur, la propager
@@ -37,7 +28,7 @@ const errorHandler = error => {
     throw error;
   }
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port; //adress est une chaine ou un num de port
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' le port nécessite des privilèges élevés');
@@ -52,8 +43,10 @@ const errorHandler = error => {
   }
 };
 
+
 // Créer serveur HTTP avec l'app Express
 const server = http.createServer(app);
+
 
 // Gestion des event de l'erreur et l'écoute du serveur
 server.on('error', errorHandler);
@@ -62,6 +55,7 @@ server.on('listening', () => {
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
+
 
 // Démarrer le serveur
 server.listen(port);
