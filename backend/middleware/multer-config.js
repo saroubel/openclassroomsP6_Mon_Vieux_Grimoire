3b0986da8f0beb1528e94404ee243507f7c9281d
saroubel <bellaf.sara@gmail.com>
2024-10-31 01:17:53 +0100

    install sharp for image optimisation

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,8 +1,12 @@
 //la configuration qui facilite la gestion des téléchargements de fichiers
 
-const multer = require('multer')    
-// const fs = require('fs');
-// const path = require('path');
+const multer = require('multer')
+const fs = require('fs')         //biblio pour modif ou supp des fichiers - file system
+const path = require('path')     //biblio pour les chemins de fichiers
+const sharp = require('sharp')   //biblio pour l'optimisation des images
+
+
+
 
 //Configuration des types MIME
 const MIME_TYPES = {
@@ -20,7 +24,8 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {                                  
     const name = file.originalname.split(' ').join('_')                 
     const namefile = name.split('.')[0]
-    callback(null, namefile + Date.now() + '.' + 'webp')                
+    callback(null, namefile + '.' + 'webp')               
+            
   }
 })
 
@@ -29,4 +34,39 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
-module.exports = {upload}
+
+//Middleware pour optimiser les images
+const optimizeImage = async (req, res, next) => { 
+
+  // Vérifié la présence de fichier
+  if (!req.file) return next(); 
+  
+  // Récupère Nom et Chemin du fichier téléchargé
+  const { filename, path: filePath } = req.file; 
+
+  try {
+    // Chemin du fichier de sortie après l'optimisation
+    const outputFilePath = path.join('images', `${path.parse(filename).name +Date.now()}.webp`)
+    sharp.cache(false)        // Désactive le cache de sharp
+    await sharp(filePath)     
+      .resize({ width: 260 })
+      .webp({ quality: 100 })
+      .toFile(outputFilePath); //save résultat dans le chemin de sortie
+
+    // Supprimer le fichier original
+    fs.unlink(filePath, err => {
+      if (err) console.error('Error Suppression de fichier original:', err)
+    });
+
+    // Mettre à jour le chemin et le nom du fichier dans la requête
+    req.file.filename = path.basename(outputFilePath)  
+    req.file.path = outputFilePath                     
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+
+module.exports = {upload, optimizeImage};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,29 +4,28 @@ const express = require('express')
 const router = express.Router()             
 const bookCtrl = require('../controllers/book') 
 const auth = require('../middleware/auth')                      
-const { upload } = require('../middleware/multer-config')       //la config multer pour stockage fichier
-
+const { upload, optimizeImage } = require('../middleware/multer-config')       //la config multer pour stockage fichier et optimisation image
 
 
 
 //POST Routes
-router.post('/', auth, upload.single('image'), bookCtrl.createBook)     //ajouter un livre
-router.post('/:id/rating', auth, bookCtrl.rateBook)                     //noter un livre
+router.post('/', auth, upload.single('image'), optimizeImage, bookCtrl.createBook)      //ajouter un livre
+router.post('/:id/rating', auth, bookCtrl.rateBook)                                     //noter un livre
 
 
 //GET Routes
-router.get('/bestrating', bookCtrl.getBestRating)                       //meilleurs livres par note
-router.get('/:id', bookCtrl.getOneBook)                                 //livre par id
-router.get('/', bookCtrl.getAllBooks)                                   //liste des livres
-
+router.get('/bestrating', bookCtrl.getBestRating)                                       //meilleurs livres par note
+router.get('/:id', bookCtrl.getOneBook)                                                 //livre par id
+router.get('/', bookCtrl.getAllBooks)                                                   //liste des livres
 
 
 //PUT Routes
-router.put('/:id', auth, upload.single('image'), bookCtrl.updateBook)   //modifier un livre par id
+router.put('/:id', auth, upload.single('image'), optimizeImage, bookCtrl.updateBook)    //modifier un livre par id
 
 
 //DELETE Routes
-router.delete('/:id', auth, bookCtrl.deleteBook)                        //supprimer un livre par id
+router.delete('/:id', auth, bookCtrl.deleteBook)                                        //supprimer un livre par id
+
 
 
 module.exports = router
